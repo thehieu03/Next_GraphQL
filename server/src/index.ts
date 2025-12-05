ec1e2fd89e83b95e3d0b5759ff39020f2a323cc3
Nguyễn Thế Hiếu <153330033+thehieu03@users.noreply.github.com>
2025-12-06 06:03:06 +0700

    add library and update thư viện create model user

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello world!");
+import "dotenv/config";
+import "reflect-metadata";
+import express from "express";
+import { DataSource } from "typeorm";
+
+const main = async () => {
+    const dataSource = new DataSource({
+        type: "postgres",
+        host: process.env.DATABASE_HOST || "localhost",
+        port: parseInt(process.env.DATABASE_PORT || "5432"),
+        database: process.env.DATABASE_NAME!,
+        username: process.env.DATABASE_USERNAME!,
+        password: process.env.DATABASE_PASSWORD!,
+        logging: process.env.NODE_ENV === "development",
+        synchronize: true,
+    });
+
+    await dataSource.initialize();
+
+    const app = express();
+
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => {
+        console.log("Server started on port " + port);
+    });
+};
+
+main().catch((err) => {
+    console.log(err);
+});
